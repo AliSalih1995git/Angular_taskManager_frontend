f4c9a1c2c51102f62b5ca920d06a315d25100909
AliSalih1995git <kvaliswalihtnl@gmail.com>
2023-08-25 07:32:37 +0530

    create auth feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserLogin } from './utilitys/loginInterface';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  submitted: boolean = false;
 
-  constructor() { }
+  error!: string;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  registerForm = this.fb.group({
+    firstName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', Validators.required],
+  });
+
+  get AllControles() {
+    return this.registerForm.controls;
+  }
+  get passwordValidation() {
+    return this.registerForm.get('password');
   }
 
+  onSubmit(value: any) {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+    const formData = this.registerForm.value;
+
+    // this.authService.register(formData).subscribe((res) => {
+    //   this.toastr.success(res.message);
+    //   this.router.navigateByUrl('/login');
+    // });
+  }
 }
---
+++ src/app/auth/utilitys/loginInterface.ts
@@ -0,0 +1,11 @@
+export interface UserLogin {
+  userName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface UserRegister {
+  email: string;
+  password: string;
+}
